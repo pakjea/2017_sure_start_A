39b28f474d6169002a812e76f10a15511b4023f1
heejin111 <glwlsl0716@gmail.com>
2017-12-20 09:46:37 +0900

    컨트롤러 변경, 빈 메인화면 생성

--- C13S/src/main/java/com/c1s3/controller/MainController.java
+++ C13S/src/main/java/com/c1s3/controller/MainController.java
@@ -1,5 +1,55 @@
 package com.c1s3.controller;
 
-public class Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+public class MainController {
+	
+	/**
+	 * 로드맵 메인
+	 */
+	@RequestMapping("/roadmapMain")
+	public String roadmapMain(Model model) {
+		
+		return "main";
+	}
+	
+	/**
+	 * 로드맵 변경이력
+	 */
+	@RequestMapping("/roadmapHistory")
+	public String roadmapHistory(Model model) {
+		
+		return "roadmapHistory";
+	}
+	
+	/**
+	 * 프로젝트 등록
+	 */
+	@RequestMapping("/registProject")
+	public String registProject(Model model) {
+		
+		return "registProject";
+	}
+	
+	/**
+	 * 프로젝트 변경
+	 */
+	@RequestMapping("/modifyProject")
+	public String modifyProject(Model model) {
+		
+		return "modifyProject";
+	}
+	
+	/**
+	 * 마일스톤 등록, 변경
+	 */
+	@RequestMapping("/mileStone")
+	public String mileStone(Model model) {
+		
+		return "mileStone";
+	}
 }
